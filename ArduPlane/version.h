a38c27c66737bc03ab5b3c941b99af2ffb58f900
samuel dudley <dudley.samuel@gmail.com>
2017-07-19 14:44:28 +0930

    plane - modify the version string

--- ArduPlane/version.h
+++ ArduPlane/version.h
@@ -2,7 +2,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduPlane V3.8.0beta6"
+#define THISFIRMWARE "ArduPlane V3.8.0beta6 sam"
 #define FIRMWARE_VERSION 3,8,0,FIRMWARE_VERSION_TYPE_BETA+5
 
 #ifndef GIT_VERSION
